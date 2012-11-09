49886ceaa1a694a28c6cc72ec17b0cbe05e3755f
Alf Eaton <alf@hubmed.org>
2012-11-09 12:55:57 +0000

    Remove the .visited eye

--- js/views.js
+++ js/views.js
@@ -157,17 +157,15 @@ var Views = {
 		},
 
 		action: function(event) {
-			var node = $(event.currentTarget);
-
 			if (event.metaKey || event.ctrlKey) {
-				node.addClass("visited");
 				return true;
 			}
+
+			var node = $(event.currentTarget);
 
 			switch (node.data("action")) {
 				case "show-abstract":
 					if (node.hasClass("expanded")) {
-						node.addClass("visited");
 						return true;
 					}
 
