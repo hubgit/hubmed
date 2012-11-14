a59cb772cf4c978f5a249e0cee0868035ae6ca96
Alf Eaton <alf@hubmed.org>
2012-11-14 08:03:09 +0000

    Don't sort by pub date

--- js/services.js
+++ js/services.js
@@ -26,7 +26,7 @@ var PubMed = function(options) {
             tool: "hubmed",
             email: "alf@hubmed.org",
             db: "pubmed",
-            sort: "pub date",
+            //sort: "pub date",
             usehistory: "y",
             retmax: 0,
             term: parts.join(" AND ")
