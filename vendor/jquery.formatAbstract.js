5b2b99e48deaee4deb5157ac90909dca622e83c5
Alf Eaton <alf@hubmed.org>
2013-08-29 16:56:03 +0100

    Update jquery.formatAbstract.js

--- vendor/jquery.formatAbstract.js
+++ vendor/jquery.formatAbstract.js
@@ -2,7 +2,7 @@
   $.fn.formatAbstract = function () {
     return this.each(function () {
       var node = $(this);
-      node.html(node.html().replace(/\.\s+([A-Z])/g, ".<br>$1"));
+      node.html("<div>" + node.html().replace(/\.\s+([A-Z])/g, ".</div><div>$1") + "</div>");
     });
   };
 })(jQuery);
