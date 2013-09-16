386dff3bb9c12923f4cd5db7770abc3d9fdebf85
Alf Eaton <alf@hubmed.org>
2013-09-16 16:58:50 +0100

    Always run some augmentors

--- js/models.js
+++ js/models.js
@@ -22,67 +22,74 @@ var Models = {
 
     Article: Backbone.Model.extend({
         augmentors: {
-            altmetric: function() {
-                var model = this,
-                    service = app.services.altmetric,
-                    data = this.toJSON(),
-                    path = service.path({ doi: data.doi, pubmed: data.pmid });
+            metrics: {
+                altmetric: function() {
+                    var model = this,
+                        service = app.services.altmetric,
+                        data = this.toJSON(),
+                        path = service.path({ doi: data.doi, pubmed: data.pmid });
 
-                if(!path) return;
+                    if(!path) return;
 
-                service.fetch(path).done(function(data) {
-                    var items = service.parse(data);
-                    model.metrics.add(items);
-                });
+                    service.fetch(path).done(function(data) {
+                        var items = service.parse(data);
+                        model.metrics.add(items);
+                    });
+                },
+
+                scopus: function() {
+                    var model = this,
+                        service = app.services.scopus,
+                        doi = this.get("doi");
+
+                    if(!doi) return;
+
+                    service.fetch(doi).done(function(data) {
+                        var item = service.parse(data);
+                        if(item) model.metrics.add(item);
+                    });
+                }
             },
+            data: {
+                oa: function() {
+                    var model = this,
+                        service = app.services.oa,
+                        oa = this.get("oa"),
+                        pmid = this.get("pmid");
 
-            scopus: function() {
-                var model = this,
-                    service = app.services.scopus,
-                    doi = this.get("doi");
+                    if(oa) return;
 
-                if(!doi) return;
+                    service.fetch(pmid).done(function(data) {
+                        var item = service.parse(data);
+                        if(item) model.set("oa", item.url);
+                    });
+                },
 
-                service.fetch(doi).done(function(data) {
-                    var item = service.parse(data);
-                    if(item) model.metrics.add(item);
-                });
-            },
+                doi: function() {
+                    var model = this,
+                        service = app.services.doi,
+                        doi = this.get("doi");
 
-            oa: function() {
-                var model = this,
-                    service = app.services.oa,
-                    oa = this.get("oa"),
-                    pmid = this.get("pmid");
+                    if(!doi) return;
 
-                if(oa) return;
-
-                service.fetch(pmid).done(function(data) {
-                    var item = service.parse(data);
-                    if(item) model.set("oa", item.url);
-                });
-            },
-
-            doi: function() {
-                var model = this,
-                    service = app.services.doi,
-                    doi = this.get("doi");
-
-                if(!doi) return;
-
-                service.fetch(doi).done(function(data) {
-                    var item = service.parse(data);
-                    if(item) model.set("url", item.url);
-                });
+                    service.fetch(doi).done(function(data) {
+                        var item = service.parse(data);
+                        if(item) model.set("url", item.url);
+                    });
+                }
             }
         },
 
         initialize: function() {
             this.metrics = new Collections.Metrics();
 
+            var model = this;
+            $.each(this.augmentors.data, function(name, augmentor) {
+                augmentor.call(model);
+            });
+
             if (app.models.options.get("metrics")) {
-                var model = this;
-                $.each(this.augmentors, function(name, augmentor) {
+                $.each(this.augmentors.metrics, function(name, augmentor) {
                     augmentor.call(model);
                 });
             }
