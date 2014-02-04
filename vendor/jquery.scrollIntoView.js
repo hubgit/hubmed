320f6e114e764f6cfad9532127196eea631b83f5
Alf Eaton <alf@hubmed.org>
2014-02-04 00:00:25 +0000

    better selected items

--- vendor/jquery.scrollIntoView.js
+++ vendor/jquery.scrollIntoView.js
@@ -1,7 +1,7 @@
 (function ($) {
 	$.fn.scrollIntoView = function () {
 		return this.each(function() {
-			$("html, body").animate({ scrollTop: $(this).offset().top - 10 }, 10);
+			$("html, body").animate({ scrollTop: $(this).offset().top - 10 }, 0);
 		});
 	};
 })(jQuery);
